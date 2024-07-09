c599da132e58a162461bb5a0c7ce66faca0a9ad4
javacasm <javacasm@gmail.com>
2024-07-09 12:23:37 +0200

    v 0.1

---
+++ code/test small font-main.py
@@ -0,0 +1,12 @@
+# Imports go at the top
+from microbit import *
+from whaley_sans_font import show_number, test_font
+
+
+
+test_font()
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    show_number(temperature())
+    sleep(1000)
---
+++ code/whaley_sans_font.py
@@ -0,0 +1,38 @@
+# makecode WhaleySans Font Package.
+# From microbit/micropython Chinese community.
+# http://www.micropython.org.cn
+# from https://github.com/makecode-extensions/WhaleySansFont/blob/master/WhaleySansFont.ts
+# adapta a python by @javacasm
+
+from microbit import display, Image
+
+v = '0.1'
+
+print('WhaleySansFont v'+v)
+
+FONT = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 1, 0, 1, 1, 1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+        [1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+        [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
+    ]
+
+def show_number(number,bright = 9,leading_cero=True):
+    display.clear()
+    if number < 0 or number > 99:
+        display.show(Image.CONFUSED)
+    else:
+        digit_10 = number // 10
+        digit_1 = number % 10
+        for i in range(5):
+            if leading_cero or digit_10>0:
+                display.set_pixel(0,i,bright*FONT[digit_10][2*i])
+                display.set_pixel(1,i,bright*FONT[digit_10][2*i+1])
+            display.set_pixel(3,i,bright*FONT[digit_1][2*i])
+            display.set_pixel(4,i,bright*FONT[digit_1][2*i+1])    
